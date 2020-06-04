c5c79e3650e8fc3c1b5fe304bf94c3f56dfd548b
Michael-Seth <michaelseth78@gmail.com>
2020-06-04 09:26:31 +0100

    chatbot with javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+//THE JS
+/*
+function onClickMenu(){
+  document.getElementById('menu-bar').classList.toggle('change');
+  document.getElementById('nav').classList.toggle('change-ctn');
+  document.getElementById('bg').classList.toggle('change-bg');
+}*/
